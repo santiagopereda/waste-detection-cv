51c9798ab99762e642bc4ebe9fffc59144ed9797
Santiago Pereda <santiago.pereda1@gmail.com>
2023-08-28 12:50:04 +0200

    intial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deep learning-based waste detection in natural and urban environments',
+    author='lewagon',
+    license='',
+)
