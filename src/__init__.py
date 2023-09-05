868b2f5bd84421b1f6af8432c0016d29c7ab4948
Santiago Pereda <33401259+santiagopereda@users.noreply.github.com>
2023-09-05 11:17:22 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 from os.path import isfile
-<<<<<<< HEAD
 from os.path import dirname
 
 version_file = '{}/version.txt'.format(dirname(__file__))
@@ -7,6 +6,3 @@ version_file = '{}/version.txt'.format(dirname(__file__))
 if isfile(version_file):
     with open(version_file) as version_file:
         __version__ = version_file.read().strip()
-=======
-from os.path import dirname
->>>>>>> aee830b28f4ffa72845e58fbda3bf534cb4885ad
